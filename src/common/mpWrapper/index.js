37237f6f682bd737e080b3608978b843fc3e5fbf
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-27 19:15:21 +0430

    add basic implementation for MapWrapper component

---
+++ src/common/mpWrapper/index.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { defaultViewPort } from '../../utils';
+
+export class MapWrapper extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: props.viewport
+    };
+  }
+
+  render() {
+    const { viewport } = this.state;
+    const { children } = this.props;
+    return <section className="map_wrapper">{children({ viewport })}</section>;
+  }
+}
+
+MapWrapper.propTypes = {
+  viewport: PropTypes.shape({
+    width: PropTypes.number,
+    height: PropTypes.number,
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    zoom: PropTypes.number
+  })
+};
+
+MapWrapper.defaultProps = {
+  viewport: defaultViewPort
+};
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const defaultViewPort = {
+  width: 400,
+  height: 400,
+  latitude: 35.72343054503254,
+  longitude: 51.38949088069012,
+  zoom: 12
+};
