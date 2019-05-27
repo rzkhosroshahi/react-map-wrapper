1040cc6106ec67a9b2c7fbf74d73756853dc1cdb
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-28 00:49:41 +0430

    add marker state in cedar map component

--- src/components/cedaMap/index.js
+++ src/components/cedaMap/index.js
@@ -1,51 +1,13 @@
 import React from 'react';
-import CedarMaps from '@cedarstudios/react-cedarmaps';
 import { MapWrapper } from '../../common/mpWrapper';
+import { RenderCedarMap } from './renderCedarMap';
 
 const accessToken = process.env.REACT_APP_CEDAR_TOKEN;
-
-const {
-  RotationControl,
-  ZoomControl,
-  ScaleControl,
-  Marker
-} = CedarMaps.getReactMapboxGl();
 
 export const CedarMap = () => {
   return (
     <MapWrapper>
-      {({
-        viewport,
-        // changeViewPort,
-        markerCords
-        // isRenderMarker,
-        // changeIsRenderMarker
-      }) => {
-        const { lat, long } = markerCords;
-        const { latitude, longitude } = viewport;
-        return (
-          <CedarMaps
-            containerStyle={{
-              height: viewport.height,
-              width: viewport.width
-            }}
-            token={accessToken}
-            center={[longitude, latitude]}
-            minZoom={viewport.zoom}
-          >
-            <RotationControl />
-            <ZoomControl />
-            <ScaleControl />
-            <Marker coordinates={[long, lat]}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
-                alt=""
-                className="pin_icon"
-              />
-            </Marker>
-          </CedarMaps>
-        );
-      }}
+      {wrapperProps => <RenderCedarMap token={accessToken} {...wrapperProps} />}
     </MapWrapper>
   );
 };
---
+++ src/components/cedaMap/renderCedarMap.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CedarMaps from '@cedarstudios/react-cedarmaps';
+
+const {
+  RotationControl,
+  ZoomControl,
+  ScaleControl,
+  Marker
+} = CedarMaps.getReactMapboxGl();
+
+export const RenderCedarMap = ({
+  viewport,
+  changeViewPortFiled,
+  changeIsRenderMarker,
+  markerCords,
+  token
+}) => {
+  const { longitude, latitude } = viewport;
+  const { long, lat } = markerCords;
+
+  const handClick = (map, evt) => {
+    changeIsRenderMarker(true);
+    changeViewPortFiled('latitude', evt.lngLat.lat);
+    changeViewPortFiled('longitude', evt.lngLat.lng);
+  };
+
+  return (
+    <div>
+      <CedarMaps
+        containerStyle={{
+          height: viewport.height,
+          width: viewport.width
+        }}
+        token={token}
+        center={[longitude, latitude]}
+        minZoom={viewport.zoom}
+        onClick={handClick}
+      >
+        <RotationControl />
+        <ZoomControl />
+        <ScaleControl />
+        <Marker
+          coordinates={[long, lat]}
+          onClick={() => changeIsRenderMarker(false)}
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
+            alt=""
+            className="pin_icon"
+          />
+        </Marker>
+      </CedarMaps>
+      <div className="ts__result">
+        <h3>Marker position</h3>
+        <p>
+          latitude: <span>{lat}</span>
+        </p>
+        <p>
+          longitude: <span>{lat}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+RenderCedarMap.propTypes = {
+  viewport: PropTypes.shape({
+    width: PropTypes.number,
+    height: PropTypes.number,
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    zoom: PropTypes.number
+  }).isRequired,
+  changeIsRenderMarker: PropTypes.func.isRequired,
+  markerCords: PropTypes.shape({
+    lat: PropTypes.number,
+    long: PropTypes.number
+  }).isRequired,
+  token: PropTypes.string.isRequired
+};
